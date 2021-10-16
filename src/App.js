042eed87d643205c05d0106620b46b8d39e0578e
Akbermet <akbermet.zh@gmail.com>
2021-10-16 22:38:09 +0600

    1.7: unicafe step2, everything except findAverage() is working

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ const Button = (props) => {
   )
 }
 
-const Statistic = ({feedbackType, feedbackCount}) => {
+const Statistic = ({statisticType, statisticCount}) => {
   return (
     <div>
-      <p> {feedbackType} {feedbackCount} </p>
+      <p> {statisticType} {statisticCount} </p>
     </div>
   )
 }
@@ -27,10 +27,23 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const totalOfAllFeedbacks = good + neutral + bad;
 
   const incrementGood = () => setGood(good + 1)
   const incrementNeutral = () => setNeutral(neutral + 1)
   const incrementBad = () => setBad(bad + 1)
+
+  const findAverage = (good, neutral, bad) => {
+    console.log(((good * 1) + (neutral * 0) + (bad  * (-1))))
+    return ((good * 1) + (neutral * 0) + (bad  * (-1))) / 3
+  }
+
+  const findPositivePercentage = (good, neutral, bad) => {
+    if(totalOfAllFeedbacks === 0)
+      return 0
+    else
+      return (100*good)/(good+neutral+bad)
+  }
 
   return (
     <div>
@@ -40,9 +53,12 @@ const App = () => {
       <Button onClick={incrementBad} text={"bad"}/>
 
       <Header title={"statistics"}/>
-      <Statistic feedbackType={"good"} feedbackCount={good}/>
-      <Statistic feedbackType={"neutral"} feedbackCount={neutral}/>
-      <Statistic feedbackType={"bad"} feedbackCount={bad}/>
+      <Statistic statisticType={"good"} statisticCount={good}/>
+      <Statistic statisticType={"neutral"} statisticCount={neutral}/>
+      <Statistic statisticType={"bad"} statisticCount={bad}/>
+      <Statistic statisticType={"all"} statisticCount={good+bad+neutral}/>
+      <Statistic statisticType={"average"} statisticCount={findAverage(good, neutral, bad)}/>
+      <Statistic statisticType={"positive"} statisticCount={findPositivePercentage(good, neutral, bad) + " %"}/>
     </div>
   )
 }
